3d7da2019f677547b7f0f6943029a3eb68c86afd
Joseph Méndez <j.mendez0434@gmail.com>
2024-02-16 12:45:03 -0500

    Update

--- script/script.js
+++ script/script.js
@@ -4,9 +4,9 @@ const whatsappCont = document.querySelector(".ws-confirmar");
 const btnCancel = document.querySelector(".ws-x");
 const btnConfirmar = document.querySelector(".btn-ws-confirmar");
 const loader = document.querySelector(".loader-cont");
-const boxes = document.querySelectorAll(".box");
-// loader que desaparecerá cuando carge la primera página
 
+
+// loader que desaparecerá cuando carge la primera página
 window.addEventListener("load", () => {
   loader.style.display = "none";
 });
@@ -25,7 +25,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3500); // cambia la imagen cada 3.5
+  setTimeout(showSlides, 6000); // cambia la imagen cada 3.5
 }
 
 
@@ -42,25 +42,7 @@ btnConfirmar.addEventListener("click", () => {
     whatsappCont.classList.remove("ws-cont-show");
 });
 
-// Animación de cuando aparecerá cada producto al hacer scroll
 
-window.addEventListener("scroll",checkBoxes);
-
-checkBoxes();
-
-function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 4;
-
-    boxes.forEach((box) => {
-        const boxTop = box.getBoundingClientRect().top;
-
-        if(boxTop < triggerBottom ) {
-            box.classList.add("show")
-        } else {
-            box.classList.remove("show");
-        }
-    })
-}
 
 
 
